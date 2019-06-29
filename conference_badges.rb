d3dc633d3fd2928f3364e5af0094e8ab9b6927d2
Don Ward <10is0420@gmail.com>
2019-06-29 15:57:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.map { |x| puts "Hello, my name is #{x}" }
+  attendees.each do |x| 
+    puts "Hello, my name is #{x}"
+  end
 end
